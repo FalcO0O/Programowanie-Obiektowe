232948c2f24c664ad80ff9e724c8db121395cbe9
FalcO0O <93099232+FalcO0O@users.noreply.github.com>
2024-12-18 01:08:16 +0100

    checkpoint

--- src/main/java/agh/ics/oop/SimulationApp.java
+++ src/main/java/agh/ics/oop/SimulationApp.java
@@ -1,29 +1,17 @@
 package agh.ics.oop;
 
-import agh.ics.oop.model.ConsoleMapDisplay;
-import agh.ics.oop.model.GrassField;
-import agh.ics.oop.presenter.SimulationPresenter;
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-public class SimulationApp extends javafx.application.Application {
-
+public class SimulationApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getClassLoader().getResource("simulation.fxml"));
         BorderPane viewRoot = loader.load();
-
-        ConsoleMapDisplay console = new ConsoleMapDisplay();
-        GrassField grassMap = new GrassField(10);
-
-        SimulationPresenter presenter = loader.getController();
-        grassMap.addObserver(presenter);
-        grassMap.addObserver(console);
-        presenter.setWorldMap(grassMap);
-
         configureStage(primaryStage, viewRoot);
         primaryStage.show();
     }
--- src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
+++ src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
@@ -3,28 +3,34 @@ package agh.ics.oop.presenter;
 import agh.ics.oop.OptionsParser;
 import agh.ics.oop.Simulation;
 import agh.ics.oop.SimulationEngine;
-import agh.ics.oop.model.MapChangeListener;
-import agh.ics.oop.model.MoveDirection;
-import agh.ics.oop.model.Vector2d;
-import agh.ics.oop.model.WorldMap;
+import agh.ics.oop.model.*;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.HPos;
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class SimulationPresenter implements MapChangeListener {
     private final static int CELL_SIZE = 30;
     private WorldMap worldMap;
-    List<Vector2d> positions = List.of(new Vector2d(2,2), new Vector2d(3,4)); // zahardocowane pozycje zwierzaków;
+    private final List<Vector2d> positions = List.of(new Vector2d(2, 2), new Vector2d(3, 4)); // Zahardcodowane pozycje zwierzaków
+    private final ExecutorService simulationExecutor = Executors.newFixedThreadPool(4); // Maksymalnie 4 symulacje równocześnie
+    private int numOfStages = 0;
 
     @FXML
     private Label moveInfoLabel;
@@ -36,7 +42,7 @@ public class SimulationPresenter implements MapChangeListener {
     private GridPane mapGrid;
 
     public void setWorldMap(WorldMap map) {
-            this.worldMap = map;
+        this.worldMap = map;
     }
 
     private void drawMap() {
@@ -49,11 +55,8 @@ public class SimulationPresenter implements MapChangeListener {
         int height = upperRight.getY() - lowerLeft.getY() + 1;
 
         addHeaders(width, height, lowerLeft);
-
         addElements(lowerLeft);
-
         setupGridConstraints(lowerLeft, upperRight);
-
         centerGrid();
     }
 
@@ -105,7 +108,7 @@ public class SimulationPresenter implements MapChangeListener {
     }
 
     private void clearGrid() {
-        mapGrid.getChildren().retainAll(mapGrid.getChildren().getFirst()); // hack to retain visible grid lines
+        mapGrid.getChildren().clear();
         mapGrid.getColumnConstraints().clear();
         mapGrid.getRowConstraints().clear();
     }
@@ -122,8 +125,42 @@ public class SimulationPresenter implements MapChangeListener {
     private void onSimulationStartClicked(ActionEvent actionEvent) {
         String[] stringOfMoves = movesListTextField.getText().split(" ");
         List<MoveDirection> listOfMoves = OptionsParser.parseMoveDirections(stringOfMoves);
-        Simulation simulation = new Simulation(positions, listOfMoves, worldMap);
-        SimulationEngine simulationEngine = new SimulationEngine(new ArrayList<>(List.of(simulation)));
-        simulationEngine.runAsync();
+
+        Platform.runLater(() -> {
+            try {
+                startNewSimulationWindow(listOfMoves);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void startNewSimulationWindow(List<MoveDirection> moves) throws IOException {
+        
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getClassLoader().getResource("simulation.fxml"));
+        BorderPane newViewRoot = loader.load();
+
+        SimulationPresenter newPresenter = loader.getController();
+        GrassField newGrassMap = new GrassField(10);
+        newGrassMap.addObserver(newPresenter);
+        newPresenter.setWorldMap(newGrassMap);
+
+        Stage newStage = new Stage();
+        configureNewStage(newStage, newViewRoot);
+
+        simulationExecutor.submit(() -> {
+            Simulation simulation = new Simulation(positions, moves, newGrassMap);
+            SimulationEngine simulationEngine = new SimulationEngine(new ArrayList<>(List.of(simulation)));
+            simulationEngine.runAsync();
+        });
+        newStage.show();
+    }
+
+    private void configureNewStage(Stage stage, BorderPane viewRoot) {
+        Scene scene = new Scene(viewRoot);
+        stage.setScene(scene);
+        stage.setTitle("Simulation nr " + ++numOfStages);
     }
 }
+
